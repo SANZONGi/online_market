816ce7e278056a171f7f601e11ffca833f579658
SANZONG <55527186+DJDNBAHICBB@users.noreply.github.com>
2021-10-25 23:59:24 +0800

    调试

--- src/main/java/com/zjgsu/online_market/config/InterceptorConfig.java
+++ src/main/java/com/zjgsu/online_market/config/InterceptorConfig.java
@@ -1,7 +1,6 @@
 package com.zjgsu.online_market.config;
 
 import com.zjgsu.online_market.interceptor.JWTInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -18,13 +17,11 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Value("${xjj.imgfilepath}")
     private String LocalImgPath;
 
-    @Autowired
-    private JWTInterceptor jwtInterceptor;
 
 //    jwt拦截
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor)
+        registry.addInterceptor(new JWTInterceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns("users/checkuser");
     }
