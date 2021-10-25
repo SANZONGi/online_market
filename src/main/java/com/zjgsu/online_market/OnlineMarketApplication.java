b4ffa39fa0721de5f465a45214bd0a8533a8be9e
SANZONG <55527186+DJDNBAHICBB@users.noreply.github.com>
2021-10-25 23:13:08 +0800

    调试

--- src/main/java/com/zjgsu/online_market/OnlineMarketApplication.java
+++ src/main/java/com/zjgsu/online_market/OnlineMarketApplication.java
@@ -3,8 +3,10 @@ package com.zjgsu.online_market;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan("com.zjgsu.online_market.mapper")
+@ComponentScan(basePackages = "com.zjgsu.online_market.interceptor")
 @SpringBootApplication
 public class OnlineMarketApplication {
     public static void main(String[] args) {
