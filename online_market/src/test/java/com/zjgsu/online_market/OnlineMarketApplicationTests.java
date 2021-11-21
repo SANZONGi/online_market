eaf85e5e08381d996903245091642c45de97187e
SANZONG <2577786020@qq.com>
2021-11-21 19:25:28 +0800

    add DateConverter

--- online_market/src/test/java/com/zjgsu/online_market/OnlineMarketApplicationTests.java
+++ online_market/src/test/java/com/zjgsu/online_market/OnlineMarketApplicationTests.java
@@ -1,16 +1,24 @@
 package com.zjgsu.online_market;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.zjgsu.online_market.common.utils.EncypterUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class OnlineMarketApplicationTests {
-//    @Autowired
-//    private EncypterUtil encypterUtil;
-//    //    private Param xjj;
-//    @Autowired
-//    private OrdersMapper ordersMapper;
-//
-//    @Test
-//    void contextLoads() {
-//    }
+    @Value("${xjj.secret}")
+    private String secret;
+    @Test
+    void contextLoads() {
+        System.out.println(secret);
+//        System.out.println(new EncypterUtil().Encrypt("111"));
+    }
 }
