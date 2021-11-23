a43a6ac4848fa188fec072d8dde06739dc2d44e4
SANZONGi <2577786020@qq.com>
2021-11-23 20:15:31 +0800

    rebuid for beauty and rewrite the entity of page , enhance the API of searching orders

--- online_market/src/main/java/com/zjgsu/online_market/controller/OrdersController.java
+++ online_market/src/main/java/com/zjgsu/online_market/controller/OrdersController.java
@@ -1,8 +1,6 @@
 package com.zjgsu.online_market.controller;
 
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.zjgsu.online_market.common.annotations.LoginRequired;
 import com.zjgsu.online_market.common.dto.PageDto;
 import com.zjgsu.online_market.common.lang.Result;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -50,47 +47,68 @@ public class OrdersController {
         return Result.success("创建成功");
     }
 
+    @ApiOperation(value = "获取订单信息（分页|指定用户|指定状态）"
+            , notes = "若不输入分页信息，则不分页\n若不输入用户则不指定用户\n若不输入状态，默认搜索全部状态")
+    @GetMapping("/orders")
+    public Result getOrders(PageDto pageDto, @Min(1) Long uid, @RequestParam(value = "status", required = false) List<Integer> status) {
+        if (pageDto.getCurrentpage() != null || pageDto.getSize() != null) {
+            if (pageDto.getCurrentpage() == null || pageDto.getSize() == null)
+                return Result.fail("分页参数不全", 1);
+        }
+        if (status != null) {
+            for (Integer i : status) {
+                if (i > OrdersServiceImpl.ORDER_FAIL || i < OrdersServiceImpl.ORDER_WAITING)
+                    return Result.fail("状态异常", 2);
+            }
+            return Result.success(ordersService.
+                    getOrdersAndUsersPageWithStatus(pageDto.getCurrentpage(), pageDto.getSize(), uid, status));
+        } else {
+            return Result.success(ordersService.
+                    getOrdersAndUsersPageWithStatus(pageDto.getCurrentpage(), pageDto.getSize(), uid, ordersService.getAllStatus()));
+        }
+    }
+
+
+    @Deprecated
     @ApiOperation("查询所有未完成订单(分页)")
     @GetMapping("/orders/unsolved")
     public Result getUnsolved(@Validated PageDto pageDto) {
-        ArrayList<Integer> statusList =new ArrayList<>(6);
+        ArrayList<Integer> statusList = new ArrayList<>(6);
         statusList.add(OrdersServiceImpl.ORDER_EXCHANGING);
         statusList.add(OrdersServiceImpl.ORDER_WAITING);
-        List<HashMap<String,String>> unsolved = ordersService.getOrdersAndUsersPageWithStatus(pageDto.getCurrentpage(), pageDto.getSize(), null,statusList);
-        return Result.success(unsolved);
+        return Result.success(ordersService.getOrdersAndUsersPageWithStatus(pageDto.getCurrentpage(), pageDto.getSize(), null, statusList));
     }
 
-
+    @Deprecated
     @ApiOperation("查询所有历史订单")
     @GetMapping("/orders/history")
     public Result allHistory(@Validated @NotNull PageDto pageDto) {
-        ArrayList<Integer> statusList =new ArrayList<>();
+        ArrayList<Integer> statusList = new ArrayList<>();
         statusList.add(OrdersServiceImpl.ORDER_FAIL);
         statusList.add(OrdersServiceImpl.ORDER_SUCCESS);
-        List<HashMap<String, String>> iPage = ordersService.getOrdersAndUsersPageWithStatus(pageDto.getCurrentpage(), pageDto
-                .getSize(), null,statusList);
-        return Result.success(iPage);
+        return Result.success(ordersService.getOrdersAndUsersPageWithStatus(pageDto.getCurrentpage(), pageDto
+                .getSize(), null, statusList));
     }
 
-
+    @Deprecated
     @ApiOperation("查询用户历史订单")
     @GetMapping("/orders/history/{uid}")
     public Result getUserHistory(@PathVariable @NotNull @Min(1) Long uid, @Validated @NotNull PageDto pageDto) {
-        ArrayList<Integer> statusList =new ArrayList<>(6);
-        statusList.add(OrdersServiceImpl.ORDER_FAIL) ;
+        ArrayList<Integer> statusList = new ArrayList<>(6);
+        statusList.add(OrdersServiceImpl.ORDER_FAIL);
         statusList.add(OrdersServiceImpl.ORDER_SUCCESS);
-        List<HashMap<String, String>> iPage = ordersService.getOrdersAndUsersPageWithStatus(pageDto.getCurrentpage(), pageDto
-                .getSize(), uid,statusList);
-        return Result.success(iPage);
+        return Result.success(ordersService.getOrdersAndUsersPageWithStatus(pageDto.getCurrentpage(), pageDto
+                .getSize(), uid, statusList));
     }
 
+    @Deprecated
     @ApiOperation("获取等待中的订单(分页)")
     @GetMapping("/orders/waiting")
     public Result getWaitingList(@Validated @NotNull PageDto pageDto) {
-        ArrayList<Integer> statusList =new ArrayList<>(6);
+        ArrayList<Integer> statusList = new ArrayList<>(6);
         statusList.add(OrdersServiceImpl.ORDER_WAITING);
         return Result.success(
-                ordersService.getOrdersAndUsersPageWithStatus(pageDto.getCurrentpage(),pageDto.getSize(),null,statusList));
+                ordersService.getOrdersAndUsersPageWithStatus(pageDto.getCurrentpage(), pageDto.getSize(), null, statusList));
     }
 
     @ApiOperation("接受订单")
--- online_market/src/test/java/com/zjgsu/online_market/OnlineMarketApplicationTests.java
+++ online_market/src/test/java/com/zjgsu/online_market/OnlineMarketApplicationTests.java
@@ -1,24 +1,20 @@
 package com.zjgsu.online_market;
 
+import com.zjgsu.online_market.mapper.OrdersMapper;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.zjgsu.online_market.common.utils.EncypterUtil;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
-
-
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class OnlineMarketApplicationTests {
-    @Value("${xjj.secret}")
-    private String secret;
+    @Autowired
+    OrdersMapper ordersMapper;
     @Test
     void contextLoads() {
-        System.out.println(secret);
-//        System.out.println(new EncypterUtil().Encrypt("111"));
+
     }
 }
