182839bf37b1bbd5fd7ba93a935be360c528007f
SANZONGi <2577786020@qq.com>
2021-12-08 21:27:18 +0800

    code style fix

--- online_market/src/main/java/com/zjgsu/online_market/controller/GoodController.java
+++ online_market/src/main/java/com/zjgsu/online_market/controller/GoodController.java
@@ -49,46 +49,49 @@ public class GoodController {
     @PutMapping("/good")
     public Result publishGood(@Validated Good good, @NotNull(message = "无图片") List<MultipartFile> files) throws IOException {
         int res = goodService.publish(good, files);
-        if (res == 0)
+        if (res == 0) {
             return Result.success("发布成功");
-        else
+        } else {
             return Result.fail("发布失败", res);
+        }
     }
 
     @ApiOperation("根据id获取商品")
     @GetMapping("/good/{gid}")
     public Result getGoodById(@PathVariable(name = "gid") Long gid) {
         Good good = goodService.getGoodById(gid);
-        if (good == null)
+        if (good == null) {
             return Result.fail("商品不存在");
+        }
         return Result.success(good);
     }
 
     @ApiOperation("根据搜索获取商品(搜索栏)")
     @GetMapping("/good/search")
-    public Result getGoodBySearch(@NotNull(message = "空参数") @NotBlank(message = "空参数") String val, @NotNull PageDto pageDto,@RequestParam("status") @NotNull @NotEmpty List<Integer> status) {
-        return Result.success(goodService.getGoodDtoListBySearch(val,pageDto,status));
+    public Result getGoodBySearch(@NotNull(message = "空参数") @NotBlank(message = "空参数") String val, @NotNull PageDto pageDto, @RequestParam("status") @NotNull @NotEmpty List<Integer> status) {
+        return Result.success(goodService.getGoodDtoListBySearch(val, pageDto, status));
     }
 
 
     @ApiOperation("根据一级类别搜索")
     @GetMapping("/good/catalogue/{pri}")
-    public Result getGoodByPri(@PathVariable @NotNull(message = "空参数") Integer pri,@NotNull PageDto pageDto,@RequestParam("status") @NotNull @NotEmpty List<Integer> status) {
+    public Result getGoodByPri(@PathVariable @NotNull(message = "空参数") Integer pri, @NotNull PageDto pageDto, @RequestParam("status") @NotNull @NotEmpty List<Integer> status) {
         new PageDto().pageValid(pageDto);
-        return Result.success(goodService.getGoodDtoListByCata(pri,null,pageDto,status));
+        return Result.success(goodService.getGoodDtoListByCata(pri, null, pageDto, status));
     }
 
     @ApiOperation("根据二级类别搜索")
     @GetMapping("/good/catalogue/{pri}/{sec}")
-    public Result getGoodBySec(@PathVariable @NotNull Integer pri, @PathVariable @NotNull Integer sec,@NotNull PageDto pageDto,@RequestParam("status") @NotNull @NotEmpty List<Integer> status) {
+    public Result getGoodBySec(@PathVariable @NotNull Integer pri, @PathVariable @NotNull Integer sec, @NotNull PageDto pageDto, @RequestParam("status") @NotNull @NotEmpty List<Integer> status) {
         new PageDto().pageValid(pageDto);
-        return Result.success(goodService.getGoodDtoListByCata(pri,sec,pageDto,status));
+        return Result.success(goodService.getGoodDtoListByCata(pri, sec, pageDto, status));
     }
 
     @ApiOperation("是否存在未下架商品")
     @GetMapping("/good/alive")
     public Result checkGoodExist() {
-        return Result.success(goodService.count(new QueryWrapper<Good>().ne("status", 2)) == 0);      //若存在商品的就返回false
+        //若存在商品的就返回false
+        return Result.success(goodService.count(new QueryWrapper<Good>().ne("status", 2)) == 0);
     }
 
     @ApiOperation("获取冻结的商品")
@@ -115,7 +118,9 @@ public class GoodController {
     @ApiOperation("查询历史商品")
     @GetMapping("/good/listofhis")
     public Result listOfHis(@Validated PageDto pageDto) {
-        if (pageDto == null) return Result.fail("传入空参数");
+        if (pageDto == null) {
+            return Result.fail("传入空参数");
+        }
         Page page = new Page(pageDto.getCurrentpage(), pageDto.getSize());
         IPage iPage = goodService.page(page, new QueryWrapper<Good>().orderByDesc("gid").eq("status", 2));
         return Result.success(200, "查询成功", iPage);
@@ -123,8 +128,8 @@ public class GoodController {
 
     @ApiOperation("根据状态获取带图片的商品列表")
     @GetMapping("/good/dto")
-    public Result getGoodDtoList(@RequestParam(value = "status",required = false) @NotNull List<Integer> status,@NotNull PageDto pageDto) {
-        return Result.success(goodService.getGoodDtoListByStatus(status,pageDto));
+    public Result getGoodDtoList(@RequestParam(value = "status", required = false) @NotNull List<Integer> status, @NotNull PageDto pageDto) {
+        return Result.success(goodService.getGoodDtoListByStatus(status, pageDto));
     }
 
 }
--- online_market/src/main/java/com/zjgsu/online_market/interceptor/JWTInterceptor.java
+++ online_market/src/main/java/com/zjgsu/online_market/interceptor/JWTInterceptor.java
@@ -18,6 +18,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+/**
+ * @author SANZONG
+ * **/
 
 @Slf4j
 @Component
@@ -36,8 +39,10 @@ public class JWTInterceptor implements HandlerInterceptor {
         LoginRequired annotation = method.getAnnotation(LoginRequired.class);
         //无此注解放行
         if (annotation == null)
+        {
             return true;
-        Map<String, Object> map = new HashMap<>();
+        }
+        Map<String, Object> map = new HashMap<>(10);
 
         //session检查
         Users users = (Users) request.getSession().getAttribute("user");
@@ -53,8 +58,8 @@ public class JWTInterceptor implements HandlerInterceptor {
 
         String token = request.getHeader("token");
         try {
-            DecodedJWT decodedJWT = jwtUtils.verify(token);
-            if (!decodedJWT.getClaim("uid").asString().equals(users.getUid().toString())) {
+            DecodedJWT decodedJwt = jwtUtils.verify(token);
+            if (!decodedJwt.getClaim("uid").asString().equals(users.getUid().toString())) {
                 log.error("用户不一致");
                 map.put("msg", "用户不一致" + "\n无法访问");
                 map.put("state", 405);
@@ -74,8 +79,9 @@ public class JWTInterceptor implements HandlerInterceptor {
             log.error(e.getMessage());
             if (token == null) {
                 map.put("msg", "token为空" + "\n请重新登录");
-            } else
+            } else {
                 map.put("msg", "token无效" + "\n请重新登录");
+            }
         }
         //Method Not Allowed
         map.put("state", 405);
