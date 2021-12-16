1975f1d4aa225da9350b81141e6c03cc1ac37198
SANZONGi <2577786020@qq.com>
2021-12-16 08:43:18 +0800

    add delete /{pri}/{sec} and put /seclist

--- online_market/src/main/java/com/zjgsu/online_market/service/ISecCataService.java
+++ online_market/src/main/java/com/zjgsu/online_market/service/ISecCataService.java
@@ -9,4 +9,25 @@ public interface ISecCataService extends IService<SecCata> {
     List<SecCata> getAll();
 
     List<SecCata> getAllInPri(Integer pri);
+
+    /**
+     * 插入一个二级列表
+     * @param secCataList
+     */
+    void addSecList(Integer pri,List<String> secCataList);
+
+    /**
+     * 插入一个二级项
+     * @param pri
+     * @param name
+     */
+    void addSec(Integer pri,String name);
+
+    /**
+     * 删除一个二级项
+     * @param pri
+     * @param sec
+     * @return
+     */
+    Integer deleteSec(Integer pri,Integer sec);
 }
--- online_market/src/main/java/com/zjgsu/online_market/service/impl/SecCataServiceImpl.java
+++ online_market/src/main/java/com/zjgsu/online_market/service/impl/SecCataServiceImpl.java
@@ -6,8 +6,8 @@ import com.zjgsu.online_market.mapper.SecCataMapper;
 import com.zjgsu.online_market.service.ISecCataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.*;
 import java.util.List;
 @Service
 public class SecCataServiceImpl extends ServiceImpl<SecCataMapper, SecCata> implements ISecCataService {
@@ -23,5 +23,28 @@ public class SecCataServiceImpl extends ServiceImpl<SecCataMapper, SecCata> impl
     public List<SecCata> getAllInPri(Integer pri) {
         return secCataMapper.getAllInPri(pri);
     }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void addSecList(Integer pri, List<String> secCataList) {
+        try {
+            for (String item : secCataList) {
+                addSec(pri, item);
+            }
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
+        }
+    }
+
+    @Override
+    public void addSec(Integer pri, String name) {
+        secCataMapper.insert(new SecCata().setPriId(pri).setName(name));
+    }
+
+    @Override
+    public Integer deleteSec(Integer pri, Integer sec) {
+        return secCataMapper.deleteSec(pri,sec);
+    }
+
 
 }
